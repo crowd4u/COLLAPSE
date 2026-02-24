1dbd4f7c4003879ce514b26192d94e22207a8386
Takumi Tamura <tamutamuphone@gmail.com>
2026-02-25 01:30:40 +0900

    exp2

---
+++ exp2/1_run_exp2_cbcc.bat
@@ -0,0 +1,11 @@
+REM This batch file runs the experiment 2 for CBCC method.
+REM First, you have to activate the enviroment with `requirements_python3_win.txt` installed.
+REM Then, run this batch file.
+
+
+python main_scripts/ds1task5/exp2_cbcc.py
+REM Note that this script should be run in this folder, which is `COLLAPSE/exp2/`.
+
+REM If you do not run this batch file 
+REM because the `python` command dose not specify the correct Python enviroment,
+REM you can replace the `python` with the full path to the Python executable in your environment.
--- exp2/run_exp2_cbcc.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-subprocess.run(["python", "main_scripts/ds1task5/exp2_cbcc.py"])
-
-
